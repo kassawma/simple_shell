c90f0ebb1bb3f2c15baa7737f3cee7fdf3e4234c
kassawma <markonayalew017@gmail.com>
2023-05-26 11:21:06 -0700

    asx

--- shell.h
+++ shell.h
@@ -1,6 +1,5 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,12 +10,10 @@
 #include <limits.h>
 #include <fcntl.h>
 #include <errno.h>
-
 /* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
-
 /* for command chaining */
 #define CMD_NORM	0
 #define CMD_OR		1
